8dcafd8279052aa848e6b2c5fce23bf994202a52
victorfernandezesp <91657222+victorfernandezesp@users.noreply.github.com>
2022-06-14 18:32:59 +0200

    Add files via upload

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ClasePerro","l":"EdadErroneaException"},{"p":"ClasePerro","l":"Mascota"},{"p":"ClasePerro","l":"Perro"}];updateSearchResults();
